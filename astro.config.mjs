62061ce764ef62ddec09844b6a79968db7fe4a43
Jian Chang <45157140+jchangz@users.noreply.github.com>
2025-09-05 23:44:23 -0600

    feat: add shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,24 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [react()],
 
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      themes: {
+        light: 'light-plus',
+        dark: 'dark-plus',
+      },
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: false,
+    },
+  },
+
   vite: {
     plugins: [tailwindcss()]
   }
